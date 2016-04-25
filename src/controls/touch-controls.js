8d7c00932e17d1de4cf33020780d2b30fffbbe5b
blerchin <ben@lerchin.com>
2016-04-24 17:54:06 -0700

    add rotation options to touch-controls

--- src/controls/touch-controls.js
+++ src/controls/touch-controls.js
@@ -1,10 +1,12 @@
 module.exports = {
   schema: {
-    enabled: { default: true }
+    enabled: { default: true },
+    sensitivity: { default: 1 / 25 }
   },
 
   init: function () {
     this.dVelocity = new THREE.Vector3();
+    this.lookVector = new THREE.Vector2();
     this.bindMethods();
   },
 
@@ -15,6 +17,7 @@ module.exports = {
   pause: function () {
     this.removeEventListeners();
     this.dVelocity.set(0, 0, 0);
+    this.lookVector.set(0, 0);
   },
 
   remove: function () {
@@ -31,6 +34,7 @@ module.exports = {
     }
 
     canvasEl.addEventListener('touchstart', this.onTouchStart);
+    canvasEl.addEventListener('touchmove', this.onTouchMove);
     canvasEl.addEventListener('touchend', this.onTouchEnd);
   },
 
@@ -39,9 +43,16 @@ module.exports = {
     if (!canvasEl) { return; }
 
     canvasEl.removeEventListener('touchstart', this.onTouchStart);
+    canvasEl.removeEventListener('touchmove', this.onTouchStart)
     canvasEl.removeEventListener('touchend', this.onTouchEnd);
   },
-
+  
+  bindMethods: function () {
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+  },
+  
   isVelocityActive: function () {
     return this.data.enabled && this.isMoving;
   },
@@ -54,14 +65,35 @@ module.exports = {
     this.dVelocity.z = this.isMoving ? -1 : 0;
     return this.dVelocity.clone();
   },
+  
+  getRotationData: function() {
+    var dRotation = this.lookVector.clone().multiplyScalar(this.data.sensitivity);
+    this.lookVector.set(0, 0);
+    return dRotation;
+  }
+  onTouchMove: function() {
+    var previousTouchEvent = this.previousTouchEvent;
 
-  bindMethods: function () {
-    this.onTouchStart = this.onTouchStart.bind(this);
-    this.onTouchEnd = this.onTouchEnd.bind(this);
+    if (!this.data.enabled || !(this.isMoving || this.pointerLocked)) {
+      return;
+    }
+
+
+    var movementY = event.screenY - previousMouseEvent.screenY;
+    var movementX = event.screenX - previousMouseEvent.screenX;
+
+    this.lookVector.x += movementX;
+    this.lookVector.y += movementY;
+
+    this.previousMouseEvent = event;
   },
+
+
 
   onTouchStart: function (e) {
     this.isMoving = true;
+    this.previousTouchEvent = event;
+
     e.preventDefault();
   },
 
